b48bbae1ad78f814f4e796459dce13d068bbe7bf
Shreyash Balpande <shreyashbalpande01@gmail.com>
2024-04-18 13:07:13 +0530

    Add files via upload

---
+++ Credentials.h
@@ -0,0 +1,19 @@
+// Provide all the crdentials here
+
+
+
+// Your WiFi Credentials 
+const char *ssid = "shreyash";       // WiFi SSID Name
+const char *password = "12345678";// WiFi Password ( Keep it blank if your WiFi router is open )
+
+
+// Parameters of Google Speech to Text API
+const String ApiKey = "AIzaSyAE_ja0vj_Z-ZF-tqCEGwfuWz4Rch9fGpg";// Google Speech to Text API key
+String SpeechtoText_Language = "en-IN"; // Language 
+
+// Parameters of OpenAI API 
+const char* chatgpt_token = "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"; // OpenAI API Key
+String OpenAI_Model = "gpt-3.5-turbo-instruct"; // Model
+String OpenAI_Temperature = "0"; // temperature
+String OpenAI_Max_Tokens = "30"; //Max Tokens 
+6666666
